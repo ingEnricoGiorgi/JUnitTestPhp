f1536710b4ffe787d2cd50005890a47d634a2a1d
ingEnricoGiorgi <enrico.giorgi92@gmail.com>
2022-11-10 19:49:49 +0100

    aggiunti criteri F.I.R.S.T.

--- tests/CleanTest/StockItemTest.php
+++ tests/CleanTest/StockItemTest.php
@@ -1,4 +1,7 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
+
 namespace CleanTest;
 
 use CleanTest\Book;
@@ -9,40 +12,41 @@ use CleanTest\Stock;
 
 final class StockItemTest extends TestCase
 {
-     /** @test */
-     public function testStockItem(): void
-     {
-         $book = new Book();
-         $bookuno= StockItem::new($book);
-         $getBeforeNumero = $bookuno->getNumero();
-         $bookuno->aggiungi();
-         $getAfterNumero= $getBeforeNumero+1;
-         //order insensitive
-         $this->assertNotEquals( $getBeforeNumero, $getAfterNumero);
-     }
-         /** @test */
-         public function testRemoveStockItem(): void
-         {
-             $book = new Book();
-             $bookuno= StockItem::new($book);
-             $getBeforeNumero = $bookuno->getNumero();
-             $bookuno->rimuovi();
-             $getAfterNumero= $getBeforeNumero-1;
-             //order insensitive
-             $this->assertEquals( $getAfterNumero, $bookuno->getNumero());
-         }
-                  /** @test */
-                  public function testZeroStockItem(): void
-                  {
-                      $book = new Book();
-                      //aggiungo un import Book con ctrl spazio sull'oggetto Book
-                      //:: si usa solo per i metodi statici senno la freccia
-                      $bookuno= StockItem::new($book);
-                      
-                      $bookuno->rimuovi();
-                      
-                      //order insensitive
-                      $this->assertEquals( 0, $bookuno->getNumero());
-                  }
-
+    /** @test */
+    public function givenCreateBook_whenAddBook_thenGetNumeroBook(): void
+    {
+        //given
+        $book = new Book();
+        $bookuno = StockItem::new($book);
+        //when
+        $getBeforeNumero = $bookuno->getNumero();
+        $bookuno->aggiungi();
+        //after
+        $getAfterNumero = $getBeforeNumero + 1;
+        $this->assertNotEquals($getBeforeNumero, $getAfterNumero);
+    }
+    /** @test */
+    public function givenCreateBook_whenRemoveBook_thenGetNumeroBook(): void
+    {
+        //given
+        $book = new Book();
+        $bookuno = StockItem::new($book);
+        $getBeforeNumero = $bookuno->getNumero();
+        //when
+        $bookuno->rimuovi();
+        $getAfterNumero = $getBeforeNumero - 1;
+        //after
+        $this->assertEquals($getAfterNumero, $bookuno->getNumero());
+    }
+    /** @test */
+    public function givenZeroBook_whenRemoveBook_thenGetNumeroBook(): void
+    {
+        //given
+        $book = new Book();
+        $bookuno = StockItem::new($book);
+        //when
+        $bookuno->rimuovi();
+        //after
+        $this->assertEquals(0, $bookuno->getNumero());
+    }
 }
--- tests/CleanTest/StockTest.php
+++ tests/CleanTest/StockTest.php
@@ -1,4 +1,7 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
+
 namespace CleanTest;
 
 use CleanTest\Book;
@@ -10,55 +13,63 @@ use Exception;
 
 final class StockTest extends TestCase
 {
-    
-/** @test */
-public function testAddBookToVoidStockItem(): void
-{
-  $book = new Book();
-  $book->setUuid(uniqid()) ->setIsbn(strval(rand(0,9999999999))) ->setTitolo("PROVA00") ->setPrezzo(28); 
-  $bookStock= new Stock;
-  $bookStock->addItem($book);
-  $arrayStock=$bookStock->getItems();
 
-  $this->assertCount(1, $arrayStock);
-}
-       /** @test */
-   public function StockRemoveBookException(): void
-   {
-    $book = new Book();
-    $book->setUuid(uniqid()) ->setIsbn(strval(rand(0,9999999999))) ->setTitolo("PROVA00") ->setPrezzo(28); 
-    $bookStock= new Stock;
-    $this->expectException(Exception::class);
-    $this->expectExceptionCode(0);
-    $bookStock->removeItem($book);
- }
-        /** @test */
-        public function StockRemoveBookException1(): void
-        {
-         $book = new Book();
-         $book->setUuid(uniqid()) ->setIsbn(strval(rand(0,9999999999))) ->setTitolo("PROVA00") ->setPrezzo(28); 
-         $book2 = new Book();
-         $book2->setUuid(uniqid()) ->setIsbn(strval(rand(0,9999999999))) ->setTitolo("PROVA01") ->setPrezzo(28); 
-            
-         $bookStock= new Stock;
-         $bookStock->addItem($book);
+    /** @test */
+ public function givenCreateBook_whenAddItem_thenGetItems(): void
+    {
+        //given
+        $book = new Book();
+        $book->setUuid(uniqid())->setIsbn(strval(rand(0, 9999999999)))->setTitolo("PROVA00")->setPrezzo(28);
+        //when
+        $bookStock = new Stock;
+        $bookStock->addItem($book);
+        //then
+        $arrayStock = $bookStock->getItems();
+        $this->assertCount(1, $arrayStock);
+    }
+    /** @test */
+    public function givenCreateBook_whenNotAddBook_thenRemoveBookItemsException0(): void
+    {
+        //given
+        $book = new Book();
+        $book->setUuid(uniqid())->setIsbn(strval(rand(0, 9999999999)))->setTitolo("PROVA00")->setPrezzo(28);
+        $bookStock = new Stock;
+        //when
 
-         $this->expectException(Exception::class);
-         $this->expectExceptionCode(1);
-         $bookStock->removeItem($book2);
-      }
-             /** @test */
-             public function StockDoubleRemoveBookException2(): void
-             {
-              $book = new Book();
-              $book->setUuid(uniqid()) ->setIsbn(strval(rand(0,9999999999))) ->setTitolo("PROVA00") ->setPrezzo(28); 
-              $bookStock= new Stock;
-              $bookStock->addItem($book);
-              $bookStock->removeItem($book);
-              $this->expectException(Exception::class);
-              $this->expectExceptionCode(2);
-              $bookStock->removeItem($book);
-           }
-
-
+        //then
+        $this->expectException(Exception::class);
+        $this->expectExceptionCode(0);
+        $bookStock->removeItem($book);
+    }
+    /** @test */
+    public function givenTwoBook_whenAddFirstBook_thenRemoveSecondBookException1(): void
+    {
+        //given
+        $book = new Book();
+        $book->setUuid(uniqid())->setIsbn(strval(rand(0, 9999999999)))->setTitolo("PROVA00")->setPrezzo(28);
+        $book2 = new Book();
+        $book2->setUuid(uniqid())->setIsbn(strval(rand(0, 9999999999)))->setTitolo("PROVA01")->setPrezzo(28);
+        //when
+        $bookStock = new Stock;
+        $bookStock->addItem($book);
+        //then
+        $this->expectException(Exception::class);
+        $this->expectExceptionCode(1);
+        $bookStock->removeItem($book2);
+    }
+    /** @test */
+    public function givenCreateBook_whenAddBookAndRemoveBook_thenRemoveBookException2(): void
+    {
+        //given
+        $book = new Book();
+        $book->setUuid(uniqid())->setIsbn(strval(rand(0, 9999999999)))->setTitolo("PROVA00")->setPrezzo(28);
+        $bookStock = new Stock;
+        //when
+        $bookStock->addItem($book);
+        $bookStock->removeItem($book);
+        //then
+        $this->expectException(Exception::class);
+        $this->expectExceptionCode(2);
+        $bookStock->removeItem($book);
+    }
 }
